3f3e9535f0f9a65423899234736c3426335221aa
Barcry573 <corybarnes10@gmail.com>
2020-09-18 14:18:35 -0400

    Create TeensyHighImpedance.ino
    Added teensy High Impedance

---
+++ hardware-tests/TeensyHighImpedance/TeensyHighImpedance.ino
@@ -0,0 +1,30 @@
+
+    #define CS_TC1 20
+    #define CS_TC2 21
+    #define MUX0 16
+    #define MUX1 17
+    #define TC1_FAULT 25
+    #define TC2_FAULT 26
+    #define SEC_CTRL1 27
+    #define SEC_CTRL2 30
+    
+void setup() {
+
+  Serial.begin(115200);
+  pinMode(SEC_CTRL1, INPUT);
+  pinMode(SEC_CTRL2, INPUT);
+  pinMode(CS_TC1, INPUT);
+  pinMode(CS_TC2, INPUT);
+  pinMode(MUX0, INPUT);
+  pinMode(MUX1, INPUT);
+  pinMode(TC1_FAULT, INPUT);
+  pinMode(TC2_FAULT, INPUT);
+  
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("All Pin in High Z");
+  
+}
