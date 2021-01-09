3f6d64882f41b6a2752a1c2e818af2caa0a6ac5e
Matt Ruffner <mattruffner7@gmail.com>
2021-01-09 15:07:27 -0500

    added start of decompress tool

---
+++ tools/decompress/main.cpp
@@ -0,0 +1,68 @@
+// decompress helper for iridium packets
+// krepe 2020 matt ruffner
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+
+#include "brieflz.h"
+
+#define MAX_DEPACKED_SIZE 5000
+
+int main(int argc, char ** argv)
+{
+
+  if( argc < 3 ){
+    std::cout << "need an input file and output file" << std::endl;
+    return 1;
+  }
+
+  std::cout << "starting" << std::endl;
+
+  std::string inFileName(argv[1]);
+  std::ifstream inFile(inFileName);
+
+  if( !inFile.is_open() ){
+    std::cout << "Could not open input file : " << inFileName << std::endl;
+    return 1;
+  }
+
+  // detect file length and read in entire file to memory   
+  inFile.seekg(0, inFile.end);
+  unsigned long length = inFile.tellg();
+  inFile.seekg(0, inFile.beg);
+
+  std::cout << "INFO: File length is " << length << " bytes" << std::endl;
+  
+  // buffer to hold data as we parse 
+  char *buffer = new char[length];
+  // keep track of current position (cp) in 
+  unsigned long cp = 0;
+
+  std::cout << "INFO: Reading in data...";
+
+  // read data as a block and close input 
+  inFile.read (buffer, length);
+  inFile.close();
+
+  std::cout << "done" << std::endl;
+
+
+  unsigned char  depacked[MAX_DEPACKED_SIZE];
+
+  std::cout << "trying to depack...";
+
+  int len = blz_depack_safe(buffer, length, depacked, 287);
+
+  std::cout << "done" << std::endl;
+
+  std::ofstream outFile("out.dat", std::ofstream::binary);
+
+  outFile.write((const char *)depacked, len);
+
+  outFile.close();
+
+  std::cout << "wrote " << len << " bytes to output file" << std::endl;
+  
+}
