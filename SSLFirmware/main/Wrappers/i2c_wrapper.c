100014e9e00310e0c4b927df58317cb22c48c6a2
Hersch Nathan <hnathan628@gmail.com>
2024-11-13 22:20:38 -0500

    begun wrapper dev, updated software requirements

---
+++ SSLFirmware/main/Wrappers/i2c_wrapper.c
@@ -0,0 +1,9 @@
+//i2c_wrapper.c
+//wraps i2c for use with freeRTOS
+//Developed for RockSat-X GHOST
+//Based off of UncleRus ESP32 I2C library
+//Developed by Hersch Nathan
+
+
+#include "i2c_wrapper.h"
+
---
+++ SSLFirmware/main/Wrappers/i2c_wrapper.h
@@ -0,0 +1,15 @@
+//i2c_wrapper.h
+//wraps i2c for use with freeRTOS
+//Developed for RockSat-X GHOST
+//Based off of UncleRus ESP32 I2C library
+//Developed by Hersch Nathan
+
+
+#ifndef I2C_WRAPPER_H
+#define I2C_WRAPPER_H
+
+
+#include <freertos/FreeRTOS.h>
+
+
+#endif
